80f4f743d73061d0df04dfc0f54fb63b2bfcfd74
Steve Gredell <steve.gredell@milliman.com>
2016-04-23 14:36:13 -0400

    Filled in patient API

--- python/prm_fhir/extractors.py
+++ python/prm_fhir/extractors.py
@@ -12,6 +12,7 @@ import typing
 from collections import OrderedDict
 
 from fhirclient.client import FHIRClient
+import fhirclient.models.patient as fhirpatient
 
 # =============================================================================
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
@@ -30,16 +31,32 @@ def _create_fhir_client(
 
 def extract_patients(
         url_fhir: str,
-        search_struct: dict,
-    ) -> typing.Generator[OrderedDict]:
+        search_struct: dict
+    ) -> "typing.Generator[OrderedDict]":
     """Generate patient records from a FHIR endpoint."""
 
     _client = _create_fhir_client(url_fhir)
 
     #TODO: Search shit
+    search_object = fhirpatient.Patient.where(search_struct)
+    bundle = search_object.perform(_client.server)
 
-    yield OrderedDict([
-        ('name', patientname),
-        ('dob', isodate),
-        ('address', blah),
-    ])
+    for patient in bundle.entry:
+        for name in patient.resource.name:
+            patientname = ", ".join([name.family[0], name.given[0]])
+        dob = patient.resource.birthDate.isostring
+        for ad in patient.resource.address:
+            address = ad.city
+
+        yield OrderedDict([
+            ('name', patientname),
+            ('dob', dob),
+            ('address', address),
+        ])
+
+if __name__ == "__main__":
+    url = 'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2'
+    search_struct = {'family':'Argonaut', 'given':'Jason'}
+    extract = extract_patients(url,search_struct)
+    for pat in extract:
+        print(pat)
