103b6376f5e1f2a844146cfc8b9a5f3b61b1ad81
skparkes <bias.variance@gmail.com>
2016-04-24 12:44:52 -0400

    Isolate EPIC and Regen lab extracts.
    Do this because their APIs are not actually the same.

--- python/prm_fhir/extractors.py
+++ python/prm_fhir/extractors.py
@@ -90,65 +90,83 @@ def extract_results(
 
     _client = _create_fhir_client(url_fhir)
 
-    with path_csv_labs.open() as csv_labs:
-        reader_labs = csv.DictReader(csv_labs)
-        for lab_record in reader_labs:
-            for patient_id in _generate_patient_fhir_ids(_client, patient_search_struct):
-                lab_search_struct = {'patient': patient_id, 'code':lab_record['loinc']}
-                search_object = fhirobs.Observation.where(lab_search_struct)
-                lab_bundle = search_object.perform(_client.server)
+    if name_fhir == 'Epic':
+        with path_csv_labs.open() as csv_labs:
+            reader_labs = csv.DictReader(csv_labs)
+            for lab_record in reader_labs:
+                for patient_id in _generate_patient_fhir_ids(_client, patient_search_struct):
+                    lab_search_struct = {'patient': patient_id, 'code':lab_record['loinc']}
+                    search_object = fhirobs.Observation.where(lab_search_struct)
+                    lab_bundle = search_object.perform(_client.server)
 
-                if lab_bundle.entry is None:
-                    continue
+                    if lab_bundle.entry is None:
+                        continue
 
-                for lab in lab_bundle.entry:
-                    try:
-                        for code in lab.resource.code.coding:
-                            if code is None:
+                    for lab in lab_bundle.entry:
+                        try:
+                            for code in lab.resource.code.coding:
+                                if code is None:
+                                    continue
+                                else:
+                                    loinc = code.code
+                        except AttributeError:
+                            print("Failed, probably trying to read an Observation as an OperationOutcome.")
+                            traceback.print_exc()
+                            continue
+
+
+
+                        try:
+                            value = lab.resource.valueQuantity.value
+                            if value is None:
                                 continue
-                            else:
-                                loinc = code.code
-                    except AttributeError:
-                        print("Failed, probably trying to read an Observation as an OperationOutcome.")
-                        traceback.print_exc()
-                        continue
-
-
-
-                    try:
-                        value = lab.resource.valueQuantity.value
-                        if value is None:
+                        except AttributeError:
+                            print("Failed, probably trying to read an Observation as an OperationOutcome.")
+                            traceback.print_exc()
                             continue
-                    except AttributeError:
-                        print("Failed, probably trying to read an Observation as an OperationOutcome.")
-                        traceback.print_exc()
-                        continue
 
-                    try:
-                        patient_name = _get_patient_name(lab.resource.subject.reference)
-                        if patient_name is None:
+                        try:
+                            patient_name = _get_patient_name(lab.resource.subject.reference)
+                            if patient_name is None:
+                                continue
+                        except AttributeError:
+                            print("Failed, probably trying to read an Observation as an OperationOutcome.")
+                            traceback.print_exc()
                             continue
-                    except AttributeError:
-                        print("Failed, probably trying to read an Observation as an OperationOutcome.")
-                        traceback.print_exc()
-                        continue
 
-                    try:
-                        date = lab.resource.effectiveDateTime.isostring
-                        if date is None:
+                        try:
+                            date = lab.resource.effectiveDateTime.isostring
+                            if date is None:
+                                continue
+                        except AttributeError:
+                            print("Failed, probably trying to read an Observation as an OperationOutcome.")
+                            traceback.print_exc()
                             continue
-                    except AttributeError:
-                        print("Failed, probably trying to read an Observation as an OperationOutcome.")
-                        traceback.print_exc()
-                        continue
 
-                    yield OrderedDict([
-                        ('name', patient_name),
-                        ('loinc', loinc),
-                        ('fhir', name_fhir),
-                        ('result', value),
-                        ('date', date)
-                    ])
+                        yield OrderedDict([
+                            ('name', patient_name),
+                            ('loinc', loinc),
+                            ('fhir', name_fhir),
+                            ('result', value),
+                            ('date', date)
+                        ])
+
+    if name_fhir == 'INPC':
+        for patient_id in _generate_patient_fhir_ids(_client, patient_search_struct):
+            lab_search_struct = {'patient': patient_id}
+            search_object = fhirobs.Observation.where(lab_search_struct)
+            lab_bundle = search_object.perform(_client.server)
+
+            assert lab_bundle.entry is not None
+            for entry in lab_bundle.entry:
+                _idx_loinc = None
+                for i, coding in enumerate(entry.resource.code.coding):
+                    if coding.system == 'http://loinc.org':
+                        _idx_loinc = i
+                if _idx_loinc is None:
+                    continue
+                yield entry
+
 
 extract_results.fieldnames = [
     'name',
@@ -172,16 +190,21 @@ def _get_patient_name(patient_url):
     return patientname
 
 if __name__ == "__main__":
+    import os
 
-    url = 'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2'
-    #url = 'http://134.68.33.32/fhir/'
-    search_struct = {'family':'Argonaut', 'given':'Jason'}
-    labs_csv = Path("c:/Users/Steve.Gredell/repos/epic-fhir/data/labs.csv")
+    #url = 'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2'
+    url = 'http://134.68.33.32/fhir/'
+    search_struct = {'family':'Argonaut'}
+    labs_csv = Path(os.environ['UserProfile']) / "repos" / "epic-fhir" / "data" / "labs.csv"
     extract = extract_patients(url,search_struct)
     for pat in extract:
         print(pat)
 
     print("Extracting labs\n\n")
-    extract_labs = extract_results(url, "Epic", search_struct, labs_csv)
+    extract_labs = extract_results(url, "INPC", search_struct, labs_csv)
     for lab in extract_labs:
-        print(lab)
+        print([code.system for code in lab.resource.code.coding])
+
+    fhirclient = _create_fhir_client(url)
+    for fhir_id in _generate_patient_fhir_ids(fhirclient, search_struct):
+        print(fhir_id)
